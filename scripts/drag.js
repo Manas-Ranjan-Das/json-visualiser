46b5373a41fefdc3aff607da4d32c68d7c753181
Manas Ranjan Das <manasranjandas.student@gmail.com>
2024-01-19 00:05:50 +0530

    The dragging function is now supported in touch screens

--- scripts/drag.js
+++ scripts/drag.js
@@ -10,21 +10,31 @@ svg.addEventListener('mousedown', startDrag);
 document.addEventListener('mousemove', drag);
 document.addEventListener('mouseup', endDrag);
 
+svg.addEventListener('touchstart', startDrag);
+document.addEventListener('touchmove', drag);
+document.addEventListener('touchend', endDrag);
 
 function startDrag(e) {
     svgGroup = svg.querySelector('g');
     isDragging = true;
     transformXY = getTransformXY(svgGroup);
-    offsetX = e.clientX - transformXY.x;
-    offsetY = e.clientY - transformXY.y;
+
+    const clientX = e.clientX || e.touches[0].clientX;
+    const clientY = e.clientY || e.touches[0].clientY;
+
+    offsetX = clientX - transformXY.x;
+    offsetY = clientY - transformXY.y;
     svg.style.cursor = 'grabbing';
 }
 
 function drag(e) {
     if (isDragging) {
-        console.log("Darging");
-        const x = e.clientX - offsetX;
-        const y = e.clientY - offsetY;
+        //console.log("Darging");
+        const clientX = e.clientX || e.touches[0].clientX;
+        const clientY = e.clientY || e.touches[0].clientY;
+
+        const x = clientX - offsetX;
+        const y = clientY - offsetY;
 
         svgGroup.setAttribute('transform', "translate("+x+","+y+")");
         //svgGroup.setAttribute('y', y);
