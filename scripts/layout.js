722fdfd9c7b35dbd025659992e027d71a6dbdf15
Manas Ranjan Das <manasranjandas.student@gmail.com>
2024-01-19 19:08:16 +0530

    layout now has close button and visualise button as absolutes and repositioned

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,5 +1,6 @@
 const mediaQuery = window.matchMedia("(orientation: portrait)");
 const closeButton = document.getElementById("inputToggle");
+visualizeButton = document.getElementById("visualize-button");
 const codeSec = document.getElementById("code-sec");
 
 if (mediaQuery.matches){
@@ -8,6 +9,16 @@ if (mediaQuery.matches){
 }
 
 closeButton.addEventListener("mousedown" , (e)=>{
+    if(codeSec.style.display != "none"){
+        codeSec.style.display = "none";
+        closeButton.innerHTML = "=";
+    }
+    else{
+        codeSec.style.display = "";
+        closeButton.innerHTML = "X"
+    }
+})
+visualizeButton.addEventListener("mousedown" , (e)=>{
     if(codeSec.style.display != "none"){
         codeSec.style.display = "none";
         closeButton.innerHTML = "=";
