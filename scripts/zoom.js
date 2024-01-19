7401753021100bcd77868b1e076a2040296edc1b
Manas Ranjan Das <manasranjandas.student@gmail.com>
2024-01-19 20:57:08 +0530

    added the zoom feature for both pc and mobile phone

---
+++ scripts/zoom.js
@@ -0,0 +1,50 @@
+//svgGroupScaler
+//svgGroupPositioner
+var currentScale ;
+var currentPosition ;
+var zoomPosition ;
+var newPosition ;
+
+var e ;
+
+
+
+svgbox.addEventListener ("wheel" , handleZoom);
+// svgbox.addEventListener("touchstart" , zoomTouchStart);
+// svgbox.addEventListener("touchmove" ,zoomTouchMove);
+
+// function zoomTouchStart (event){
+//     if(event.touches.length == 2){
+
+//     }
+// }
+
+// function zoomTouchMove(event){
+
+// }
+
+
+
+function handleZoom(event) {
+    e = event ;
+    event.preventDefault();
+    console.log(event);
+    zoomPosition = [event.offsetX ,event.offsetY]
+
+    const newScale = event.deltaY < 0 ? zoomBy (1.1) : zoomBy (1 / 1.1);
+    
+}
+
+
+function zoomBy (scale){
+    currentScale = getTransformScale(svgGroupScaler);
+    currentPosition = getTransformXY(svgGroupPositioner);
+    
+    const newScale =  currentScale * scale;
+    newPosition = [ zoomPosition[0] + (currentPosition.x - zoomPosition[0] ) * scale ,  
+                    zoomPosition[1] + (currentPosition.y - zoomPosition[1] ) * scale ]
+    
+    svgGroupPositioner.setAttribute('transform', "translate("+newPosition[0]+","+newPosition[1]+")");
+
+    svgGroupScaler.setAttribute("transform","scale("+newScale + ")");
+}
